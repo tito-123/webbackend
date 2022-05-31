e4420deb53458e78097e5c2e5f70ea77bcdcf4cd
tito-123 <titolunafin1@gmail.com>
2022-05-31 07:53:09 -0400

    generator 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        Schema::defaultStringLength(191);
+      //  Schema::defaultStringLength(191);
         if (env('REDIRECT_HTTPS')) {
             $url->formatScheme('https://');
         }
